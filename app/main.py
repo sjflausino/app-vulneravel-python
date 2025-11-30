a4967478f3656710f856651ba0fd7ff941e35eb1
Sandro Flausino <sjflausino@gmail.com>
2025-11-30 16:01:15 -0300

    feat: removendo comentarios

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-# --- CONFIGURAÇÃO DO BANCO DE DADOS ---
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app_fastapi.db"
 
 engine = create_engine(
@@ -14,22 +13,20 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# --- MODELO DO BANCO DE DADOS ---
 class UserDB(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String)
-    password = Column(String)  # <--- NOVO CAMPO (Vulnerável: Texto plano para o TCC)
+    password = Column(String)
 
-# --- ESQUEMAS DO PYDANTIC (Validação) ---
 class UserCreate(BaseModel):
     username: str
     email: str
-    password: str  # <--- Agora exigimos senha na criação
+    password: str
 
-class UserLogin(BaseModel):  # <--- Novo esquema para o Login
+class UserLogin(BaseModel):  
     username: str
     password: str
 
@@ -41,7 +38,6 @@ class UserResponse(BaseModel):
     class Config:
         orm_mode = True
 
-# --- INICIALIZAÇÃO ---
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
@@ -53,16 +49,13 @@ def get_db():
     finally:
         db.close()
 
-# --- ROTAS ---
 
 @app.post("/users/", response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    # Verifica duplicidade
     db_user = db.query(UserDB).filter(UserDB.username == user.username).first()
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
     
-    # Cria usuário (Atenção: Salvando senha em texto puro para fins didáticos/TCC)
     new_user = UserDB(username=user.username, email=user.email, password=user.password)
     db.add(new_user)
     db.commit()
@@ -74,13 +67,10 @@ def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     users = db.query(UserDB).offset(skip).limit(limit).all()
     return users
 
-# --- NOVA ROTA DE LOGIN ---
 @app.post("/login")
 def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
-    # Busca o usuário pelo nome
     user = db.query(UserDB).filter(UserDB.username == user_credentials.username).first()
 
-    # Verifica se usuário existe e se a senha bate
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     
